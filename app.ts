9acc7e95d36dde834f15137ad9fa8827e9789751
Anna Sidorova <anna.sidorova@equaleyes.com>
2022-04-22 22:43:00 +0200

    feat: add tests & util functions for handling responses

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 import http from "http";
 
 import * as got from "./data.json";
+import { sendHttpError, sendHttpResponse } from "./utils";
 
 const port = process.env.PORT || 3000;
 
@@ -8,23 +9,32 @@ const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "application/json");
 
   if (req.method !== "GET") {
-    res.end(`{"error": "${http.STATUS_CODES[405]}"}`);
+    sendHttpError(res, { status: 404 });
   }
   const [_, resourse, id] = req.url?.split("/") || [];
 
   if (resourse === "characters") {
     if (!id) {
       // return all characters
-      res.end(JSON.stringify(got.characters));
+      sendHttpResponse(res, { data: got.characters });
+      return;
     }
-    res.end(
-      JSON.stringify(got.characters.find((character) => character.id == id))
-    );
+
+    const character = got.characters.find((character) => character.id == id);
+    if (character) {
+      sendHttpResponse(res, {
+        data: character,
+      });
+    } else {
+      sendHttpError(res, { status: 404, message: "Character not found" });
+    }
   } else {
-    res.end(`{"error": "${http.STATUS_CODES[404]}"}`);
+    sendHttpError(res, { status: 404 });
   }
 });
 
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+export default server;
---
+++ tests/integration/health.test.ts
@@ -0,0 +1,12 @@
+import request from "supertest";
+import { expect } from "chai";
+
+import app from "../../app";
+
+describe("general api testing", () => {
+  it("should return 404 error if endpoint does not exist", async () => {
+    const response = await request(app).get("/non-existant");
+
+    expect(response.status).to.equal(404);
+  });
+});
